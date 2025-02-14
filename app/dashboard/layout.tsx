4e811d31a66b20c98ddf19af34a144346f793bcc
Omkumar_87 <135239304+Omkumar7Parmar@users.noreply.github.com>
2025-02-15 00:48:31 +0530

    moving layout file and about folder to (root)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Layout = ({children} :{children: React.ReactNode}) => {
+const Layout = ({ children } :{children: React.ReactNode}) => {
   return (
       <div>
           <h1 className="text-3xl">HEADER for dashboard</h1>
+          {children}
       </div>
   )
 }
